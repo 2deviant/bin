24e6e6b0c7fb861b20a9005660bb37bd18953b9d
Val Tenyotkin <val@tenyotk.in>
2013-10-28 01:12:57 -0700

    Initial commit.

---
+++ fast/add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    FILE *f;
+    long double sum = 0, x;
+
+    // if the file name is not specified, attempt to read from the standard
+    // input
+    if(!(f = argc <= 1 ? stdin : fopen(argv[1], "r"))) {
+        printf("Unable to open stream for reading.\n");
+        return 1;
+    }
+
+    while(1) {
+
+        // read the next number
+        fscanf(f, "%Lf", &x);
+
+        // if the end of the file has been reached, stop
+        if(feof(f))
+            break;
+
+        sum += x;
+    }
+
+    printf("%Lg\n", sum);
+
+    return 0;
+}
