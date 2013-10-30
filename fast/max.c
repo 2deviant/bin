7791cfdd8a3185e2061ebc01c409a002232f6837
Val Tenyotkin <val@tenyotk.in>
2013-10-30 02:04:06 -0700

    Initial commit.

---
+++ fast/max.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    FILE *f;
+    long double max = -1e100, x;
+
+    // if the file name is not specified, attempt to read from the standard
+    // input
+    if(!(f = argc <= 1 ? stdin : fopen(argv[1], "r"))) {
+        printf("Unable to open stream for reading.\n");
+        return 1;
+    }
+
+    while(1) {
+
+        // read the next number
+        fscanf(f, "%Lf", &x);
+
+        // if the end of the file has been reached, stop
+        if(feof(f))
+            break;
+
+        if(x > max)
+            max = x;
+    }
+
+    printf("%.10Lg\n", max);
+
+    return 0;
+}
---
+++ fast/mean.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    FILE *f;
+    long double sum = 0, x, n = 0;
+
+    // if the file name is not specified, attempt to read from the standard
+    // input
+    if(!(f = argc <= 1 ? stdin : fopen(argv[1], "r"))) {
+        printf("Unable to open stream for reading.\n");
+        return 1;
+    }
+
+    while(1) {
+
+        // read the next number
+        fscanf(f, "%Lf", &x);
+
+        // if the end of the file has been reached, stop
+        if(feof(f))
+            break;
+
+        sum += x;
+        n ++;
+    }
+
+    printf("%.10Lg\n", sum/n);
+
+    return 0;
+}
